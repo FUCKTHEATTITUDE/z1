5bd849d827902857cbb958606382f4ecbaf228c5
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-31 15:07:15 +0530

    Update restricted.py

--- bot/restricted.py
+++ bot/restricted.py
@@ -8,7 +8,7 @@ def restricted(func):
         user_id = update.effective_user.id
         if user_id != int(userId):
             print("Unauthorized access denied for {0}. Allowed users: {1}".format(user_id,userId))
-            context.bot.send_message(chat_id=user_id, text="You are not authorized to use this bot! Make your own YeetMeet bot from https://github.com/1337w0rm/YeetMeet")
+            context.bot.send_message(chat_id=user_id, text="You are not authorized to use this bot! Make your own zoom member bot contact @alpha_romeo_06")
             return
         return func(update, context, *args, **kwargs)
     return wrapped
