bc43568b2eee21f7f9d618165fdad31bda714aa2
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-31 16:29:39 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,23 +3,20 @@ from telegram.ext import Updater
 from config import Config
 from .restricted import restricted
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 
 try:
- BASE_DIR = os.path.dirname(os.path.realpath(__file__))
- print(BASE_DIR)
- s = BASE_DIR + '/Profile/'
- if not os.path.exists(s):
-  os.makedirs(s)
- print (s)
- q = '/app/vendor/firefox/firefox'
- binary = FirefoxBinary(q)
+ 
  updater = Updater(token = Config.BOT_TOKEN, use_context=True)
  dp = updater.dispatcher
- options = webdriver.FirefoxOptions()
+ options = webdriver.ChromeOptions()
  options.add_argument("-profile")
  options.add_argument(s)
  #profile = FirefoxProfile()
@@ -27,12 +24,12 @@ try:
  #profile.set_preference('useAutomationExtension', False)
  #profile.set_preference("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/60.0.3112.50 Safari/537.36")
  #profile.update_preferences()
- desired = DesiredCapabilities.FIREFOX
+ desired = DesiredCapabilities.Chrome
  options.add_argument("--disable-infobars")
  options.add_argument("--headless")
  #options.add_argument("--window-size=1200,800")
  options.add_argument("--disable-blink-features=AutomationControlled")
- browser = webdriver.Firefox(options=options, executable_path='/app/vendor/geckodriver', firefox_binary=binary, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
+ browser = webdriver.Chrome(options=options, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
  #browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 except Exception as e:
  print(e)
